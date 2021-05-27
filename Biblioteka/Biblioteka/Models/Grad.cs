37743314db9e8a19dd69be31ecc783b9bd1727a7
unknown <mbektasevi1@etf.unsa.ba>
2021-05-28 01:03:21 +0200

    doradjeni modeli

---
+++ Biblioteka/Biblioteka/Models/Grad.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biblioteka.Models
+{
+    public class Grad
+    {
+        [Key]
+        [Required]
+        public int id { get; set; }
+
+        [Required]
+
+        public string naziv { get; set; }
+
+        [Foreignkey("Biblioteka")]
+        [Required]
+
+        public int biblioteka_id { get; set; }
+    }
+}
---
+++ Biblioteka/Biblioteka/Models/Korisnik.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biblioteka.Models
+{
+    public class Korisnik : Osoba
+    {
+
+        [ForeignKey("Osoba")]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public List<Knjiga> pozajmljene_knjige { get; set; }
+
+        [Required]
+        private long broj_ziror_acuna { get; set; }
+
+        [Required]
+        private DateTime datum_isteka_clanarine { get; set; }
+
+
+    }
+}
